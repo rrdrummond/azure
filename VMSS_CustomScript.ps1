144e63432ebad0dca452ad062352577b79ed8b9e
Drummond <37912915+rrdrummond@users.noreply.github.com>
2018-03-29 16:50:52 -0300

    Add files via upload

---
+++ VMSS_CustomScript.ps1
@@ -0,0 +1,24 @@
+﻿# Define the script for your Custom Script Extension to run
+$publicSettings = @{
+    "fileUris" = (,"https://raw.githubusercontent.com/Azure-Samples/compute-automation-configurations/master/automate-iis.ps1");
+    "commandToExecute" = "powershell -ExecutionPolicy Unrestricted -File automate-iis.ps1"
+}
+
+# coletar informações referente ao VMSS
+$vmss = Get-AzureRmVmss `
+            -ResourceGroupName "myResourceGroup" `
+            -VMScaleSetName "myScaleSet"
+
+# Usando extensão para execução de um script personalizado (Instalação do IIS com site basico)
+Add-AzureRmVmssExtension -VirtualMachineScaleSet $vmss `
+    -Name "customScript" `
+    -Publisher "Microsoft.Compute" `
+    -Type "CustomScriptExtension" `
+    -TypeHandlerVersion 1.8 `
+    -Setting $publicSettings
+
+# Atualizar modelo VMSS e instancias com script personalizado.
+Update-AzureRmVmss `
+    -ResourceGroupName "myResourceGroup" `
+    -Name "myScaleSet" `
+    -VirtualMachineScaleSet $vmss
