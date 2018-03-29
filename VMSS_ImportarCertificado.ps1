44555394580cf1a3b9d92c0cbec4f3e8a84a5b4a
Drummond <37912915+rrdrummond@users.noreply.github.com>
2018-03-29 16:58:50 -0300

    Add files via upload

---
+++ VMSS_ImportarCertificado.ps1
@@ -0,0 +1,4 @@
+﻿$mypwd = Get-Credential -UserName 'Enter password below' -Message 'Enter password below'
+
+Import-PfxCertificate -FilePath C:\Certificados\wildcard-portaldeassinaturas-com-br.pfx `
+-CertStoreLocation Cert:\LocalMachine\My -Password $mypwd.Password -Exportable
