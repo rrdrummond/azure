2a421338fa38835949c22f213c0674cda1bec4f4
Drummond <37912915+rrdrummond@users.noreply.github.com>
2018-04-02 15:18:52 -0300

    Delete VMSS_ImportarCertificado.ps1

--- VMSS_ImportarCertificado.ps1
+++
@@ -1,4 +0,0 @@
-﻿$mypwd = Get-Credential -UserName 'Enter password below' -Message 'Enter password below'
-
-Import-PfxCertificate -FilePath C:\Certificados\wildcard-portaldeassinaturas-com-br.pfx `
--CertStoreLocation Cert:\LocalMachine\My -Password $mypwd.Password -Exportable
